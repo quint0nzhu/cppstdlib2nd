b9ffa4265310bcf8955d3bb8e3e4f7cc029ea9d5
quint0nzhu <1935842336@qq.com>
2018-07-08 23:39:48 +0800

    6.2.1 is doing!

--- test.cpp
+++ test.cpp
@@ -5,10 +5,23 @@
 */
 
 #include <iostream>
+#include <vector>
 
 
 int main()
 {
+  std::vector<int> coll; //vector container for integer elements
+
+  //append elements with values 1 to 6
+  for(int i = 1; i <= 6; ++i){
+    coll.push_back(i);
+  }
+
+  //print all elements followed by a space
+  for(int i = 0; i < coll.size(); ++i){
+    std::cout << coll[i] << ' ';
+  }
+  std::cout << std::endl;
 
   return 0;
 }
