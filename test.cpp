656bcb9eda1122844d49085171f188cad9fec10a
quint0nzhu <1935842336@qq.com>
2018-07-09 23:56:39 +0800

    deque is done!

--- test.cpp
+++ test.cpp
@@ -6,10 +6,15 @@
 
 #include <iostream>
 #include <vector>
+#include <deque>
 
 
 int main()
 {
+  //6.
+  //6.1 STL
+  //6.2 Container
+  //6.2.1 Sequence Container
   std::vector<int> coll; //vector container for integer elements
 
   //append elements with values 1 to 6
@@ -22,6 +27,20 @@ int main()
     std::cout << coll[i] << ' ';
   }
   std::cout << std::endl;
+
+  std::deque<float> coll1;//deque container for floating-point elements
+
+  //insert elements from 1.1 to 6.6 each at the front
+  for(int i = 1; i <= 6; ++i){
+    coll1.push_front(i*1.1);//insert at the front
+  }
+
+  //print all elements followed by a space
+  for(int i = 0; i < coll1.size(); ++i){
+    std::cout << coll1[i] << ' ';
+  }
+  std::cout << std::endl;
+
 
   return 0;
 }
