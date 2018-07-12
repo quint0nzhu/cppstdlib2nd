4af134d13e6a1ced131ec558530ade6c40cef837
quint0nzhu <1935842336@qq.com>
2018-07-12 23:56:59 +0800

    6.2.1 is done!

--- test.cpp
+++ test.cpp
@@ -7,6 +7,19 @@
 #include <iostream>
 #include <vector>
 #include <deque>
+#include <array>
+#include <list>
+#include <forward_list>
+
+
+
+template<typename T>
+void printElements(const T& coll)
+{
+  for(const auto& elem : coll){
+    std::cout << elem << std::endl;
+  }
+}
 
 
 int main()
@@ -41,6 +54,57 @@ int main()
   }
   std::cout << std::endl;
 
+  //array container of 5 string elements:
+  std::array<std::string,5> coll2={"hello","world"};
+
+  //print each element with its index on a line
+  for(int i = 0; i < coll2.size(); ++i) {
+    std::cout << i << ": " << coll2[i]<<std::endl;
+  }
+
+  std::list<char> coll3; // list container for character elements
+
+  //append elements from 'a' to 'z'
+  for(char c = 'a'; c <= 'z'; ++c){
+    coll3.push_back(c);
+  }
+
+  //print all elements:
+  //-use range-based for loop
+  for(auto elem : coll3){
+    std::cout << elem << ' ';
+  }
+  std::cout<<std::endl;
+
+  std::list<char> coll4; //list container for character elements
+
+  //append elements from 'a' to 'z'
+  for(char c = 'a'; c <= 'z'; ++c){
+    coll4.push_back(c);
+  }
+
+  //print all elements
+  //- while there are elements
+  //- print and remove the first element
+  while(!coll4.empty()){
+    std::cout << coll4.front() <<' ';
+    coll4.pop_front();
+  }
+  std::cout<<std::endl;
+
+  //create forward-list container for some prime numbers
+  std::forward_list<long> coll5={2,3,5,7,11,13,17};
+
+  //resize two times
+  //-note: poor performance
+  coll5.resize(9);
+  coll5.resize(10, 99);
+
+  //print all elements:
+  for(auto elem : coll5){
+    std::cout << elem << ' ';
+  }
+  std::cout << std::endl;
 
   return 0;
 }
