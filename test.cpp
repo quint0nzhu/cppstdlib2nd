7a50ffb20348b85547dfa82ac3f4c9d579816124
18607942672 <18607942672@163.com>
2018-07-24 04:50:40 -0400

    7.1.2 is doing!

--- test.cpp
+++ test.cpp
@@ -5,11 +5,43 @@
 */
 
 #include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <list>
 
 
 int main()
 {
- 
+  //7 STL容器
+  //7.1容器的共通能力和共通操作
+  //7.1.1容器的共通能力
+  //7.1.2容器的共通操作
+
+  //initialize a vector with some specific values (since C++11)
+  const std::vector<int> v1 = {1,2,3,5,7,11,13,17,21};
+  for(const auto& elem : v1){
+    std::cout << elem << ' ';
+  }
+  std::cout << std::endl;
+
+  //same with different syntax
+  const std::vector<int> v2 {1,2,3,5,7,11,13,17,21};
+  for(const auto& elem : v2){
+    std::cout << elem << ' ';
+  }
+  std::cout << std::endl;
+
+  //initialize an unordered set with "hello" and two empty strings
+  std::unordered_set<std::string> w = {"hello",std::string(),""};
+  for(const auto& elem : w){
+    std::cout << elem << std::endl;
+  }
+  std::cout << std::endl;
+
+  
+
+
+
 
   return 0;
 }
