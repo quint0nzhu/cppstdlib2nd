54aef37229d18e5d9c329f780e24aa93e0fdff92
quint0nzhu <1935842336@qq.com>
2018-07-24 20:37:18 +0800

    cin is done!

--- test.cpp
+++ test.cpp
@@ -8,6 +8,9 @@
 #include <vector>
 #include <unordered_set>
 #include <list>
+#include <deque>
+#include <set>
+#include <iterator>
 
 
 int main()
@@ -38,9 +41,51 @@ int main()
   }
   std::cout << std::endl;
 
-  
+  std::list<int> l={1,2,3,4,5,6};//l is a linked list of ints
+
+  //copy all elements of the list as floats into a vector
+  std::vector<float> c(l.begin(),l.end());
+
+  for(const auto& elem : c){
+    std::cout << elem << ' ';
+  }
+  std::cout << std::endl;
+
+  std::list<std::string> l1{"hello","world","!"};//l1 is a linked list of strings
+  //move all elements of the list into a vector
+  std::vector<std::string> c1(std::make_move_iterator(l1.begin()),
+                              std::make_move_iterator(l1.end()));
+
+  std::cout << "print moved elements in vector:  " << std::endl;
+  for(const auto& elem : c1){
+    std::cout << elem << std::endl;
+  }
 
 
+  std::cout << "print origin elements in list:" << std::endl;
+  for(const auto& elem : l1){
+    std::cout << "elements in list is null" << std::endl;
+    std::cout << elem << std::endl;
+  }
+
+  int carray[] = {2,3,17,33,45,77};
+
+  //copy all elements of the C-style array into a set
+  std::set<int> c2(std::begin(carray),std::end(carray));
+  //std::set<int> c2(carray,carray+sizeof(carray)/sizeof(carray[0]));//C++98
+
+  for(const auto& elem : c2){
+    std::cout << elem << ' ';
+  }
+  std::cout << std::endl;
+
+  //read all integer elements of the deque from standard input
+  std::deque<int> c3{std::istream_iterator<int>(std::cin),
+      std::istream_iterator<int>()};
+
+  for(const auto& elem : c3){
+    std::cout << elem << std::endl;
+  }
 
 
   return 0;
