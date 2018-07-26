67ea278ef4a29df607458618a0b15f6985389e11
18607942672 <18607942672@163.com>
2018-07-26 04:41:25 -0400

    7.2.1 is starting!

--- test.cpp
+++ test.cpp
@@ -18,7 +18,9 @@ int main()
   //7 STL容器
   //7.1容器的共通能力和共通操作
   //7.1.1容器的共通能力
+  //所有容器提供的都是value语义而非reference语义
   //7.1.2容器的共通操作
+  //初始化最好统一用{}
 
   //initialize a vector with some specific values (since C++11)
   const std::vector<int> v1 = {1,2,3,5,7,11,13,17,21};
@@ -80,12 +82,46 @@ int main()
   std::cout << std::endl;
 
   //read all integer elements of the deque from standard input
-  std::deque<int> c3{std::istream_iterator<int>(std::cin),
-      std::istream_iterator<int>()};
+  //std::deque<int> c3{std::istream_iterator<int>(std::cin),
+  //    std::istream_iterator<int>()};
+  std::deque<int> c3((std::istream_iterator<int>(std::cin)),
+                     (std::istream_iterator<int>()));
 
   for(const auto& elem : c3){
     std::cout << elem << std::endl;
   }
+
+  std::vector<int> v3{1,3,5,7,9};
+
+  //move contents of v3 to v4, state of v3 undefined afterward
+  std::vector<int> v4=std::move(v3);
+  //如果赋值后右侧的内容不再被使用，都建议用上面这种赋值方法
+  for(const auto& elem : v4){
+    std::cout << elem << ' ';
+  }
+  std::cout << std::endl;
+  for(const auto& elem : v3){
+    std::cout << elem << ' ';
+  }
+  std::cout << std::endl;
+
+  //尽量用empty(）
+
+  std::cout << v4.size() << " " << v4.max_size() << std::endl;
+  //两个容器的所有元素依序相等，那么这两个容器相等
+  //cbegin(),cend()只读，begin(),end()可读可写，这些函数与auto配合使用
+  //colltype::const_iterator pos;//C++98用法，只读
+  //colltype::iterator pos;//C++98用法，可读可写
+  //删除元素后vector和deque的迭代器或指向元素的reference可能失效
+  //clear()移除所有元素后，对vector,deque,string而言，由end()或cend()返回的逾尾迭代器都可能失效
+  //插入元素后，list,forward_list和关联式容器保证原本的迭代器和指向元素的reference继续保持有效，当插入vector且不超出容量时，以上结果也适用于vector，对于无序(unordered)容器，上述保证对于reference一般是成立的，但对于迭代器则只有当不发生rehashing才成立，只要插入后的最终元素总数小于bucket个数乘以最大负载系数，就不会发生rehashing
+
+  //7.1.3容器提供的类型
+  //pointer和const_pointer是C++11的新引入的
+
+  //7.2 Array
+
+
 
 
   return 0;
