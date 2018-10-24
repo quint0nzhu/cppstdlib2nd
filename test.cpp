28dd59d85488484ab8bd44e47f677430323029c0
18607942672 <18607942672@163.com>
2018-10-24 05:39:52 -0400

    7.7.4 is done!

--- test.cpp
+++ test.cpp
@@ -87,6 +87,32 @@ find_before_if(InputIterator first, InputIterator last, Pred pred)
   return first;
 }
 
+class RuntimeCmp{
+public:
+  enum cmp_mode{normal,reverse};
+private:
+  cmp_mode mode;
+public:
+  //constructor for sorting criterion
+  //-default criterion uses value normal
+  RuntimeCmp (cmp_mode m=normal):mode(m){}
+
+  //comparison of elements
+  //-member function for any element type
+  template<typename T>
+  bool operator()(const T& t1, const T& t2)const{
+    return mode==normal?t1<t2:t2<t1;
+  }
+
+  //comparison of sorting criteria
+  bool operator==(const RuntimeCmp& rc)const{
+    return mode==rc.mode;
+  }
+};
+
+//type of a set that uses this sorting criterion
+typedef std::set<int,RuntimeCmp> IntSet;
+
 
 
 int main()
@@ -692,9 +718,143 @@ int main()
   std::cout<<"equal_range(5): "<<*c6.equal_range(5).first<<" "
            <<*c6.equal_range(5).second<<std::endl;
 
+  std::set<double> c7;
 
+  c7.insert(1.1);
+  c7.insert(2.2);
+  c7.insert(3.3);
+  c7.insert(4.4);
 
+  if(c7.insert(3.3).second){
+    std::cout<<"3.3 inserted"<<std::endl;
+  }
+  else{
+    std::cout<<"3.3 already exists"<<std::endl;
+  }
 
+  //insert value and process return value
+  double value = 4.4;
+  auto status=c7.insert(value);
+  if(status.second){
+    std::cout<< value <<" inserted as element ";
+  }
+  else{
+    std::cout<< value <<" already exists as element ";
+  }
+  std::cout<<std::distance(c7.begin(),status.first)+1<<std::endl;
+
+  std::set<int> coll10;
+  coll10.insert(1);
+  coll10.insert(3);
+  coll10.insert(5);
+
+  //remove all elements with passed value
+  std::cout<<coll10.erase(4)<<std::endl;
+  PRINT_ELEMENTS(coll10);
+
+  std::multiset<int> coll11={1,2,3,3,3,4,5,4,2};
+  PRINT_ELEMENTS(coll11);
+
+  //remove first element with passed value
+  std::multiset<int>::iterator pos3;
+  pos3=coll11.find(4);//this find() fast
+  if(pos3!=coll11.end()){
+    coll11.erase(pos3);
+  }
+  std::cout<<coll11.erase(3)<<std::endl;
+  PRINT_ELEMENTS(coll11);
+
+  //7.7.3 异常处理(Exception Handling)
+  //7.7.4 Set和Multiset运用实例
+
+  //type of the collection:
+  //-no duplicates
+  //-elements are integral values
+  //-descending order
+  std::set<int,std::greater<int>> coll12;
+
+  //insert elements in random order using different member functions
+  coll12.insert({4,3,5,1,6,2});
+  coll12.insert(5);
+
+  //print all elements
+  for(int elem : coll12){
+    std::cout<<elem<<' ';
+  }
+  std::cout<<std::endl;
+
+  //insert 4 again and process return value
+  auto status1=coll12.insert(4);
+  if(status1.second){
+    std::cout<<"4 inserted as element "<<std::distance(coll12.begin(),status1.first)+1<<std::endl;
+  }
+  else{
+    std::cout<<"4 already exists"<<std::endl;
+  }
+
+  //assign elements to another set with ascending order
+  std::set<int> coll13(coll12.cbegin(),coll12.cend());
+
+  //print all elements of the copy using stream iterators
+  copy(coll13.cbegin(),coll13.cend(),
+       std::ostream_iterator<int>(std::cout, " "));
+  std::cout<<std::endl;
+
+  //remove all elements up to element with value 3
+  coll13.erase(coll13.begin(),coll13.find(3));
+
+  //remove all elements with value 5
+  int num;
+  num = coll13.erase(5);
+  std::cout<<num<<" element(s) removed"<<std::endl;
+
+  //print all elements
+  copy(coll13.cbegin(),coll13.cend(),
+       std::ostream_iterator<int>(std::cout, " "));
+  std::cout<<std::endl;
+
+  //type of the collection:
+  //-can duplicates
+  //-elements are integral value
+  //-descending order
+  std::multiset<int,std::greater<int>> coll14;
+
+  //insert elements in random order using different member functions
+  coll14.insert({4,3,5,1,6,2});
+  coll14.insert(5);
+
+  //print all elements
+  for(int elem : coll14){
+    std::cout<<elem<<' ';
+  }
+  std::cout << std::endl;
+
+  //insert 4 again and process return value
+  auto ipos=coll14.insert(4);
+  std::cout<<"4 inserted as element "<<std::distance(coll14.begin(),ipos)+1<<std::endl;
+
+  //assign elements to another multiset with ascending order
+  std::multiset<int> coll15(coll14.cbegin(),coll14.cend());
+
+  //print all elements of the copy using stream iterators
+  copy(coll15.cbegin(),coll15.cend(),
+       std::ostream_iterator<int>(std::cout," "));
+  std::cout<<std::endl;
+
+  //remove all elements up to element with value 3
+  coll15.erase(coll15.begin(),coll15.find(3));
+
+  //remove all elements with value 5
+  std::cout << coll15.erase(5)<<" element(s) removed"<<std::endl;
+
+  //print all elements
+  PRINT_ELEMENTS(coll15);
+
+  //7.7.5 运行期指定排序准则
+
+  //create, fill, and print set with normal element order
+  //-uses default sorting criterion
+  IntSet coll16={4,7,5,1,6,2,5};
 
 
 
