5396b18e2ff945dd82f352f97983b7d6a1d393a4
quinton <18607942672@163.com>
2018-10-26 18:48:03 +0800

    new work place!

--- test.cpp
+++ test.cpp
@@ -115,6 +115,30 @@ public:
 typedef std::set<int,RuntimeCmp> IntSet;
 
 
+namespace MyLib{
+  template<typename Cont>
+  inline
+  bool replace_key(Cont& c,
+                    const typename Cont::key_type& old_key,
+                    const typename Cont::key_type& new_key)
+  {
+    typename Cont::iterator pos;
+    pos = c.find(old_key);
+    if(pos != c.end()){
+      //insert new element with value of old element
+      c.insert(typename Cont::value_type(new_key,pos->second));
+
+      //remove old element
+      c.erase(pos);
+      return true;
+    }
+    else{
+      //key not found
+      return false;
+    }
+  }
+}
+
 
 int main()
 {
