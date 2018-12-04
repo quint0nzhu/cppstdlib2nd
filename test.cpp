c95a66e720b8cdb26c0d67627f0927b02267d14c
18607942672 <18607942672@163.com>
2018-12-04 04:17:45 -0500

    16.2 is starting!

--- test.cpp
+++ test.cpp
@@ -7,6 +7,20 @@
 
 int main(int argc, char* argv[])
 {
+  //第16章 国际化
+  //16.1 字符编码和字符集
+  //16.1.1 多字节(Multibyte)和宽字符(Wide-Character)文本
+  //16.1.2 不同的字符集
+  //16.1.3 在C++中处理字符集
+  //16.1.4 Character Trait
+  //16.1.5 特殊字符的国际化
+
+  //strm.widen('\n');//internationalized newline character
+  //strm.widen('\0');//internationalized string-termination character
+  //std::use_facet<std::ctype<charType>>(loc).widen(c);
+
+  //16.2 Locale（地域）概念
+  
 
   return 0;
 }
